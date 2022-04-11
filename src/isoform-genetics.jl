7f28431087cd10394a2bf0b46a2556fc6380f7db
Minsoo <mmkim1210@gmail.com>
2022-04-10 22:08:06 -0700

    first commit

---
+++ src/isoform-genetics.jl
@@ -0,0 +1,161 @@
+using CSV, DataFrames, CairoMakie, RCall, ColorSchemes
+using SnpArrays, GLM, JLD, Arrow, Distributions
+using GeneticsMakie, MultiResponseVarianceComponentModels
+using LinearAlgebra, Statistics
+const GM = GeneticsMakie
+const MRVC = MultiResponseVarianceComponentModel
+const MRVCs = MultiResponseVarianceComponentModels
+
+CairoMakie.activate!(type = "png")
+set_theme!(;
+    fontsize = 6, 
+    font = "Arial",
+    Axis = (
+        xticksize = 3,
+        xticklabelsize = 6,
+        yticksize = 3,
+        yticklabelsize = 6,
+        xlabelsize = 6,
+        ylabelsize = 6,
+        xtickalign = 0,
+        ytickalign = 0,
+        xtickwidth = 0.75,
+        ytickwidth = 0.75,
+        xgridwidth = 0.25,
+        ygridwidth = 0.25,
+        spinewidth = 0.75,
+        xticklabelpad = 0,
+        yticklabelpad = 2
+        )
+    )
+
+mutable struct Gene
+    gene_name          :: String
+    gene_id            :: String
+    chr                :: String
+    start              :: Real
+    stop               :: Real
+    all_isoforms       :: Vector{String}
+    expressed_isoforms :: Vector{String}
+    heritable_isoforms :: Union{Nothing, Vector{Int}}
+    gene_heritable     :: Bool
+    Yg                 :: Vector{Float64}
+    Yi                 :: Union{Vector{Float64}, Matrix{Float64}}
+    X                  :: Matrix{Float64} # covariates
+    snparray           :: Union{Nothing, Matrix{Float64}} # cis SNPs only
+    snpinfo            :: Union{Nothing, DataFrame}
+    grmcis             :: Union{Nothing, Matrix{Float64}}
+    grmtrans           :: Union{Nothing, Matrix{Float64}}
+    ncisnps            :: Union{Nothing, Int}
+end
+
+function Gene(
+    gene     :: String,
+    gencode  :: DataFrame,
+    expr     :: DataFrame,
+    expri    :: DataFrame,
+    X        :: Union{Nothing, Matrix{Float64}},
+    window   :: Real,
+    geno     :: Union{Nothing, SnpData},
+    whichgrm :: Union{Nothing, String},
+    )
+
+    if startswith(gene, "ENSG")
+        gene_id = gene
+        gene_name = gencode.gene_name[findfirst(isequal(gene), gencode.gene_id)]
+    else
+        gene_id = gencode.gene_id[findfirst(isequal(gene), gencode.gene_name)]
+        gene_name = gene
+    end
+    chr, start, stop = GM.findgene(gene_name, gencode)
+    df = filter(x -> (x.gene_id == gene_id) && (x.feature .== "transcript"), gencode)
+    all_isoforms = df.transcript_id
+    df = filter(x -> x.pid == gene_id, expr)
+    Yg = permutedims(Matrix{Float64}(df[:, 7:end]))
+    size(Yg, 2) == 1 ? Yg = vec(Yg) : nothing
+    df = filter(x -> x.pid == gene_id, expri)
+    Yi = permutedims(Matrix{Float64}(df[:, 7:end]))
+    size(Yi, 2) == 1 ? Yi = vec(Yi) : nothing
+    expressed_isoforms = df.gid
+    heritable_isoforms = nothing
+    gene_heritable = false
+    # isnothing(X) ? X = ones(size(Yg, 1), 1) : nothing
+    if !isnothing(geno) && isfile(joinpath(@__DIR__, "../data/$(gene_name)-cis.bed"))
+        genocis = SnpData(joinpath(@__DIR__, "../data/$(gene_name)-cis"))
+        snparray, snpinfo = (convert(Matrix{Float64}, genocis.snparray), genocis.snp_info)
+        ncisnps = size(genocis.snparray, 2)
+    elseif !isnothing(geno)
+        SnpArrays.filter(geno; des = joinpath(@__DIR__, "../data/$(gene_name)-cis"), 
+            f_snp = x -> (x[:chromosome] == chr && x[:position] > (start - window) && x[:position] < (stop + window)))
+        genocis = SnpData(joinpath(@__DIR__, "../data/$(gene_name)-cis"))
+        snparray, snpinfo = (convert(Matrix{Float64}, genocis.snparray), genocis.snp_info)
+        ncisnps = size(genocis.snparray, 2)
+    else
+        snparray, snpinfo, ncisnps = nothing, nothing, nothing
+    end
+    if whichgrm == "cis"
+        grmcis = grm(genocis.snparray, method = :GRM)
+        grmcis .*= 2
+        grmtrans = nothing
+    elseif whichgrm == "both"
+        grmcis = grm(genocis.snparray, method = :GRM)
+        grmcis .*= 2
+        if !isfile(joinpath(@__DIR__, "../data/$(gene_name)-trans.bed"))
+            SnpArrays.filter(geno; des = joinpath(@__DIR__, "../data/$(gene_name)-trans"), 
+                f_snp = x -> !(x[:chromosome] == chr && x[:position] > (start - window) && x[:position] < (stop + window)))
+        end
+        genotrans = SnpData(joinpath(@__DIR__, "../data/$(gene_name)-trans"))
+        grmtrans = grm(genotrans.snparray, method = :GRM)
+        grmtrans .*= 2
+    else
+        grmcis = grmtrans = nothing
+    end
+    Gene(
+        gene_name, gene_id, chr, start, stop, 
+        all_isoforms, expressed_isoforms, heritable_isoforms,
+        gene_heritable, Yg, Yi, X, snparray, snpinfo, grmcis, grmtrans, ncisnps
+        )
+end
+
+@info "Loading gene expression data"
+if !isfile(joinpath(@__DIR__, "../data/expression/PsychENCODE-EUR-gene.BED.arrow"))
+    @time expr = CSV.read(joinpath(@__DIR__, "../data/expression/PsychENCODE-EUR-gene.BED.gz"), delim = "\t", DataFrame)
+    select!(expr, Not([:CMC_MSSM_307, :CMC_PITT_077, :Br1977, :Br1876, :Br2173]))
+    @time Arrow.write(joinpath(@__DIR__, "../data/expression/PsychENCODE-EUR-gene.BED.arrow"), expr)
+end
+@time expr = Arrow.Table(joinpath(@__DIR__, "../data/expression/PsychENCODE-EUR-gene.BED.arrow")) |> DataFrame
+@assert (24_905, 861) == size(expr)
+
+@info "Loading isoform expression data"
+if !isfile(joinpath(@__DIR__, "../data/expression/PsychENCODE-EUR-isoform.BED.arrow"))
+    @time expri = CSV.read(joinpath(@__DIR__, "../data/expression/PsychENCODE-EUR-isoform.BED.gz"), delim = "\t", DataFrame)
+    select!(expri, Not([:CMC_MSSM_307, :CMC_PITT_077, :Br1977, :Br1876, :Br2173]))
+    @time Arrow.write(joinpath(@__DIR__, "../data/expression/PsychENCODE-EUR-isoform.BED.arrow"), expri)
+end
+@time expri = Arrow.Table(joinpath(@__DIR__, "../data/expression/PsychENCODE-EUR-isoform.BED.arrow")) |> DataFrame
+@assert (93_293, 861) == size(expri)
+
+@info "Loading covariates for expression"
+@time cov = CSV.read(joinpath(@__DIR__, "../data/expression/PsychENCODE-EUR-covariates.tsv"), delim = "\t", DataFrame)
+cov = cov[Not([257, 439, 665, 757, 831]), :]
+cov = Matrix(cov)
+@assert (855, 45) == size(cov)
+
+@info "Loading GENCODE annotation"
+if !isfile(joinpath(@__DIR__, "../data/gencode.v19.annotation.parsed.gtf.arrow"))
+    run(`curl http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz --output ./data/gencode.v19.annotation.gtf.gz`)
+    @time gencode = CSV.read(joinpath(@__DIR__, "../data/gencode.v19.annotation.gtf.gz"), DataFrame,
+        delim = "\t", skipto = 6, header = ["seqnames", "source", "feature", "start", 
+        "end", "score", "strand", "phase", "info"])
+    GeneticsMakie.parsegtf!(gencode)
+    select!(gencode, :seqnames, :feature, :start, :end, :strand, :gene_id, :gene_name, :gene_type, :transcript_id)
+    @time Arrow.write(joinpath(@__DIR__, "../data/expression/gencode.v19.annotation.parsed.gtf.arrow"), gencode)
+    run(`rm ./data/gencode.v19.annotation.gtf.gz`)
+end 
+@time gencode = Arrow.Table(joinpath(@__DIR__, "../data/gencode.v19.annotation.parsed.gtf.arrow"))|> DataFrame
+@assert (2_619_444, 9) == size(gencode)
+
+@info "Loading genotype data"
+@time geno = SnpData(joinpath(@__DIR__, "../data/genotype/Capstone4.HRC.European.unique.frontal.nochr.filter.unrelated"))
+@assert (855, 4_685_674) == size(geno.snparray)
+@assert names(expri)[7:end] == geno.person_info.iid
---
+++ src/vc.jl
@@ -0,0 +1,208 @@
+include(joinpath(@__DIR__, "isoform-genetics.jl"))
+
+function fit_uni(Y, X, V)
+    model = MRVC(Y, X, V)
+    MRVCs.fit!(model, maxiter = 3000, reml = true)
+    h², h²se = calculate_h2(model)
+    model_mle = MRVC(Y, X, V)
+    MRVCs.fit!(model_mle, maxiter = 3000)
+    h²_mle, h²se_mle = calculate_h2(model_mle)
+    model0 = MRVC(Y, X, V[end])
+    MRVCs.fit!(model0, maxiter = 3000)
+    p = calculate_p(model_mle, model0)
+    vcat(
+        [model.Σ[k][1] for k in 1:3], 
+        [sqrt(model.Σcov[k, k]) for k in 1:3],
+        vec(h²), vec(h²se),
+        [model_mle.Σ[k][1] for k in 1:3], 
+        [sqrt(model_mle.Σcov[k, k]) for k in 1:3],
+        vec(h²_mle), vec(h²se_mle), p
+        )
+end
+
+function runvc_uni!(gene::Gene)
+    @info "Fitting univariate, three variance components model"
+    V = [gene.grmcis, gene.grmtrans, Matrix{Float64}(I, size(gene.grmcis))]
+    @info "Fitting gene"
+    result = permutedims(fit_uni(gene.Yg, gene.X, V))
+    d = size(gene.Yi, 2)
+    for i in 1:d
+        @info "Fitting $i / $d isoform"
+        result = vcat(result, permutedims(fit_uni(gene.Yi[:, i], gene.X, V)))
+    end
+    result = DataFrame(result, [
+        :var_cis_reml, :var_trans_reml, :var_res_reml, :se_var_cis_reml,
+        :se_var_trans_reml, :se_var_res_reml, :h2_cis_reml, :h2_trans_reml,
+        :h2_reml, :se_h2_cis_reml, :se_h2_trans_reml, :se_h2_reml, 
+        :var_cis_mle, :var_trans_mle, :var_res_mle, :se_var_cis_mle, 
+        :se_var_trans_mle, :se_var_res_mle, :h2_cis_mle, :h2_trans_mle,
+        :h2_mle, :se_h2_cis_mle, :se_h2_trans_mle, :se_h2_mle, :p
+        ])
+    result.gene_name .= gene.gene_name
+    result.gene_id .= gene.gene_id
+    result.id = vcat(gene.gene_id, gene.expressed_isoforms)
+    result.feature = vcat("gene", fill("isoform", d))
+    result.ncisnps .= gene.ncisnps
+    select!(result, :gene_name, :gene_id, :id, :feature, :)
+    result.p[1] < 0.05 ? gene.gene_heritable = true : nothing
+    ind = findall(result.p[2:end] .< 0.05)
+    !isempty(ind) ? gene.heritable_isoforms = ind : nothing
+    CSV.write(joinpath(@__DIR__, "../results/univariate/$(gene.gene_id).tsv"), result, delim = "\t")
+end
+
+function runvc_bi(gene::Gene)
+    if isnothing(gene.heritable_isoforms) || length(gene.heritable_isoforms) == 1
+        return
+    end
+    result = DataFrame(
+        gene_name           = String[],
+        gene_id             = String[],
+        id                  = String[],
+        pair                = String[],
+        var_cis_reml        = Float64[],
+        covar_cis_reml      = Float64[],
+        var_trans_reml      = Float64[],
+        covar_trans_reml    = Float64[],
+        var_res_reml        = Float64[],
+        covar_res_reml      = Float64[],
+        se_var_cis_reml     = Float64[],
+        se_covar_cis_reml   = Float64[],
+        se_var_trans_reml   = Float64[],
+        se_covar_trans_reml = Float64[],
+        se_var_res_reml     = Float64[],
+        se_covar_res_reml   = Float64[],
+        h2_cis_reml         = Float64[],
+        h2_trans_reml       = Float64[],
+        h2_reml             = Float64[],
+        se_h2_cis_reml      = Float64[],
+        se_h2_trans_reml    = Float64[],
+        se_h2_reml          = Float64[],
+        rg_cis_reml         = Float64[],
+        rg_trans_reml       = Float64[],
+        re_reml             = Float64[],
+        se_rg_cis_reml      = Float64[],
+        se_rg_trans_reml    = Float64[],
+        se_re_reml          = Float64[],
+        p_cis               = Float64[],
+        p_trans             = Float64[],
+        p_res               = Float64[],
+        ncisnps             = Int[]
+        )
+    V = [gene.grmcis, gene.grmtrans, Matrix{Float64}(I, size(gene.grmcis))]
+    d = length(gene.heritable_isoforms)
+    @info "Fitting bivariate, three variance components model for $d isoforms"
+    for i in 1:d
+        for j in (i + 1):d
+            @info "Fitting $i, $j pair"
+            model = MRVC(gene.Yi[:, [gene.heritable_isoforms[i], gene.heritable_isoforms[j]]], gene.X, V)
+            MRVCs.fit!(model, maxiter = 3000, reml = true) 
+            h², h²se = calculate_h2(model)
+            r₉, r₉se = calculate_rg(model)
+            r₉_parsed = [r₉[k][1, 2] for k in 1:3]
+            r₉se_parsed = [r₉se[k][1, 2] for k in 1:3]
+            zs = (r₉_parsed ./ r₉se_parsed).^2
+            ps = ccdf.(Chisq(1), zs)
+            push!(result, vcat(
+                gene.gene_id,
+                gene.gene_name,
+                gene.expressed_isoforms[gene.heritable_isoforms[i]],
+                gene.expressed_isoforms[gene.heritable_isoforms[j]],
+                reduce(vcat, [model.Σ[k][1:2] for k in 1:3]),
+                sqrt.(diag(model.Σcov)[[1, 2, 4, 5, 7, 8]]),
+                h²[:, 1], h²se[:, 1],
+                r₉_parsed,
+                r₉se_parsed,
+                ps, 
+                gene.ncisnps
+                ))
+            push!(result, vcat(
+                gene.gene_id,
+                gene.gene_name,
+                gene.expressed_isoforms[gene.heritable_isoforms[j]],
+                gene.expressed_isoforms[gene.heritable_isoforms[i]],
+                reduce(vcat, [model.Σ[k][4:-1:3] for k in 1:3]),
+                sqrt.(diag(model.Σcov)[[3, 2, 6, 5, 9, 8]]),
+                h²[:, 2], h²se[:, 2],
+                r₉_parsed,
+                r₉se_parsed,
+                ps, 
+                gene.ncisnps
+                ))    
+        end
+    end
+    CSV.write(joinpath(@__DIR__, "../results/bivariate/$(gene.gene_id).tsv"), result, delim = "\t")
+end
+
+function runvc_mul(gene::Gene)
+    d = length(gene.heritable_isoforms)
+    if isnothing(gene.heritable_isoforms) || d < 3
+        return
+    end
+    np = (d * (d + 1)) >> 1
+    ind = ones(Int, d)
+    for i in 2:length(ind)
+        ind[i] = ind[i - 1] + (d - i + 2) 
+    end
+    V = [gene.grmcis, gene.grmtrans, Matrix{Float64}(I, size(gene.grmcis))]
+    d = length(gene.heritable_isoforms)
+    @info "Fitting multivariate, three variance components model for $d isoforms"
+    model = MRVC(gene.Yi[:, gene.heritable_isoforms], gene.X, V)
+    @time MRVCs.fit!(model, maxiter = 3000, reml = true, verbose = true)
+    h², h²se = calculate_h2(model)
+    r₉, r₉se = calculate_rg(model)
+    h2rg = reduce(hcat, [MRVCs.vech(r₉[i]) for i in 1:3])
+    h2rgse = reduce(hcat, [MRVCs.vech(r₉se[i]) for i in 1:3])
+    for i in 1:3
+        h2rg[ind, i] = h²[i, :]
+        h2rgse[ind, i] = h²se[i, :]
+    end
+    zs = (h2rg ./ h2rgse).^2
+    ps = ccdf.(Chisq(1), zs)
+    ps[ind, :] .= NaN
+    result = hcat(
+        reduce(hcat, [MRVCs.vech(model.Σ[i]) for i in 1:3]),
+        reduce(hcat, [diag(model.Σcov)[((i - 1) * np + 1):(i * np)] for i in 1:3]),
+        h2rg, h2rgse, ps
+        )
+    result = DataFrame(result, [
+        "var/covar_cis_reml", "var/covar_trans_reml", "var/covar_res_reml", 
+        "se_var/covar_cis_reml", "se_var/covar_trans_reml", "se_var/covar_res_reml",
+        "h2/rg_cis_reml", "h2/rg_trans_reml", "h2/re_reml",
+        "se_h2/rg_cis_reml", "se_h2/rg_trans_reml", "se_h2/re_reml", 
+        "p_cis", "p_trans", "p_res"
+        ])
+    ids = gene.expressed_isoforms[gene.heritable_isoforms]
+    txind = MRVCs.vech([[j, i] for i in 1:d, j in 1:d])
+    ids = reduce(vcat, [permutedims(ids[txind[i]]) for i in 1:length(txind)])    
+    result.gene_name .= gene.gene_name
+    result.gene_id .= gene.gene_id
+    result.id = ids[:, 1]
+    result.pair = ids[:, 2]
+    result.ncisnps .= gene.ncisnps
+    select!(result, :gene_name, :gene_id, :id, :pair, :)
+    CSV.write(joinpath(@__DIR__, "../results/multivariate/$(gene.gene_id).tsv"), result, delim = "\t")
+end
+
+for folder in ["univariate", "bivariate", "multivariate"]
+    ispath(joinpath(@__DIR__, "../results/$folder")) || 
+        mkpath(joinpath(@__DIR__, "../results/$folder"))
+end
+
+function main(gencode, expr, expri, cov, geno)
+    for geneid in union(expr.pid, expri.pid)[52:end]
+        isfile(joinpath(@__DIR__, "../results/univariate/$(geneid).tsv")) && continue
+        @info "Working on $(geneid)"
+        @time gene = Gene(geneid, gencode, expr, expri, cov, 1e6, geno, "both")
+        @time runvc_uni!(gene)
+        @time runvc_bi(gene)
+        @time runvc_mul(gene)
+        for plink in ["bed", "bim", "fam"]
+            rm("data/$(gene.gene_name)-cis.$(plink)")
+            rm("data/$(gene.gene_name)-trans.$(plink)")
+        end
+        # add try catch for numerically unstable cases
+        # add parser
+    end
+end
+
+main(gencode, expr, expri, cov, geno)
