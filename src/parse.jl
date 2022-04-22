e1a25ea64c8abb2f9dfed6496bb1b875b65a1f4b
mmkim1210 <mmkim1210@gmail.com>
2022-04-21 18:43:29 -0700

    parse results

--- src/parse.jl
+++ src/parse.jl
@@ -2,16 +2,16 @@ using CSV, DataFrames
 
 @info "Parsing multivariate results"
 files = readdir("results/multivariate")
-uni = Vector{DataFrame}(undef, length(files))
+mul = Vector{DataFrame}(undef, length(files))
 for i in 1:length(files)
     rem(i, 100) == 0 ? println("parsed $(i) genes so far") : nothing
-    uni[i] = CSV.read("results/multivariate/$(files[i])", delim = "\t", DataFrame)
+    mul[i] = CSV.read("results/multivariate/$(files[i])", delim = "\t", DataFrame)
 end
-uni = reduce(vcat, uni)
-df = combine(groupby(uni, :gene_name), nrow => :count)
+mul = reduce(vcat, mul)
+df = combine(groupby(mul, :gene_name), nrow => :count)
 sort(df, order(:count, rev = true))
 df[df.count .== 21, :]
-CSV.write("results/multivariate.tsv", uni, delim = "\t")
+CSV.write("results/multivariate.tsv", mul, delim = "\t")
 
 @info "Parsing pairwise bivariate results"
 files = readdir("results/bivariate")
@@ -21,14 +21,15 @@ for i in 1:length(files)
     bi[i] = CSV.read("results/bivariate/$(files[i])", delim = "\t", DataFrame)
 end
 bi = reduce(vcat, bi)
+rename!(bi, :gene_id => :gene_name, :gene_name => :gene_id)
 CSV.write("results/bivariate.tsv", bi, delim = "\t")
 
 @info "Parsing univariate results"
 files = readdir("results/univariate")
-mul = Vector{DataFrame}(undef, length(files))
+uni = Vector{DataFrame}(undef, length(files))
 for i in 1:length(files)
     rem(i, 100) == 0 ? println("parsed $(i) genes so far") : nothing
-    mul[i] = CSV.read("results/univariate/$(files[i])", delim = "\t", DataFrame)
+    uni[i] = CSV.read("results/univariate/$(files[i])", delim = "\t", DataFrame)
 end
-mul = reduce(vcat, mul)
-CSV.write("results/univariate.tsv", mul, delim = "\t")
+uni = reduce(vcat, mul)
+CSV.write("results/univariate.tsv", uni, delim = "\t")
