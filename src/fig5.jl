d765c7a59385e7dbf9c65a8d68810c777b01fbfe
Minsoo <mmkim1210@gmail.com>
2022-04-29 16:38:24 -0700

    modify fig5

--- src/fig5.jl
+++ src/fig5.jl
@@ -24,30 +24,69 @@ set_theme!(;
         )
     )
 
-
-@time gencode = CSV.read(joinpath(@__DIR__, "../results/XRN2/gffcmp.XRN2.filtered.gtf"), DataFrame,
-    delim = "\t", skipto = 5, header = ["seqnames", "source", "feature", "start", 
+@time gencode = CSV.read(joinpath(@__DIR__, "../results/long-read/gffcmp.filtered.gencode.gtf.gz"), DataFrame,
+    delim = "\t", skipto = 4, header = ["seqnames", "source", "feature", "start", 
     "end", "score", "strand", "phase", "info"]) # hg38 coordinates
-gencode.gene_id .= "ENSG00000088930"
-gencode.gene_name .= "XRN2"
-storage = Vector(undef, size(gencode, 1))
-for (n, m) in enumerate(match.(Regex("transcript_id \"(.*?)\";"), gencode.info))
-    if isnothing(m)
-        storage[n] = gencode.gene_id[n]
-    else
-        storage[n] = getindex(m.captures, 1)
-    end
-end
-gencode[:, :transcript_id] = storage
 gencode.seqnames = replace.(gencode.seqnames, "chr" => "")
+for col in ["transcript_id", "gene_id", "gene_name", "cmp_ref"]
+    storage = Vector(undef, size(gencode, 1))
+    for (n, m) in enumerate(match.(Regex("$(col) \"(.*?)\";"), gencode.info))
+        if isnothing(m)
+            storage[n] = missing
+        else
+            storage[n] = getindex(m.captures, 1)
+        end
+    end
+    gencode[:, col] = storage
+end
+for col in ["num_samples"]
+    storage = Vector(undef, size(gencode, 1))
+    for (n, m) in enumerate(match.(Regex("$(col) (.*?);"), gencode.info))
+        if isnothing(m)
+            storage[n] = missing
+        else
+            storage[n] = getindex(m.captures, 1)
+        end
+    end
+    gencode[:, col] = storage
+end
+for col in ["cmp_ref_gene"]
+    storage = Vector(undef, size(gencode, 1))
+    for (n, m) in enumerate(match.(Regex("$(col) \"(.*?)\""), gencode.info))
+        if isnothing(m)
+            storage[n] = missing
+        else
+            storage[n] = getindex(m.captures, 1)
+        end
+    end
+    gencode[:, col] = storage
+end
+# gencode.cmp_ref_gene = [getindex(i, 1) for i in split.(gencode.cmp_ref_gene, ".")]
+# gencode.cmp_ref = [getindex(i, 1) for i in split.(gencode.cmp_ref, ".")]
 
+xrn2 = gencode[findall(isequal("ENSG00000088930.8"), gencode.cmp_ref_gene), :]
+xrn2 = gencode[findall(in(xrn2.transcript_id), gencode.transcript_id), :]
+xrn2.gene_name .= "XRN2"
+xrn2[findall(isequal("TCONS_00530677"), xrn2.transcript_id), :]
+nsamples = Int[]
+for isoform in unique(xrn2.transcript_id)
+    push!(nsamples, parse(Int, xrn2[findfirst(isequal(isoform), xrn2.transcript_id), :num_samples]))
+end
 @time let
     f = Figure(resolution = (306, 792))
     ax = Axis(f[1, 1])
-    rs, chr, range1, range2 = GM.plotisoforms!(ax, "XRN2", gencode; height = 0.1,
-        highlight = (["TCONS_00000039"], ["#CB3C33"]))
+    rs, chr, range1, range2 = GM.plotisoforms!(ax, "XRN2", xrn2; height = 0.1,
+        highlight = (["TCONS_00530677"], ["#CB3C33"]))
     GM.labelgenome(f[1, 1, Bottom()], chr, range1, range2)
     rowsize!(f.layout, 1, rs)
     resize_to_layout!(f)
     save("figs/XRN2-long-read.png", f, px_per_unit = 4)
 end
+
+# q1: GENCODE/gencode.v40.annotation.gtf
+# q2: Patowary/Isoform_annotations_pseudoBulk.GRCh38.no_gene_names.gtf
+# q3: Chun/ALL_combined_isoSeqccs_demux_flnc.5merge.collapsed.rep_classification.filtered_lite.gtf
+# q4: GTEx/flair_filter_transcripts.gtf
+# q5: “Mill/Filtered - Primary dataset/HumanCTX_sqantitamafiltered.final.classification.gtf”
+# q6: PacBio/Alzheimer_IsoSeq2019.postFilter.gtf
+# q7: PacBio/UHR_IsoSeq.gff
